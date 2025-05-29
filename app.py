68dd773df1d9e8bda18b6419b3647d47ee0e94e4
unknown <kinlievenicedeguzman@gmail.com>
2025-05-30 02:00:29 +0800

    backend done

--- app.py
+++ app.py
@@ -167,45 +167,6 @@ def get_subject_tasks(subject_id):
         cursor.close()
         conn.close()
 
-# @app.route('/classification/<string:classname>/subjects', methods=['GET'])
-# def get_class_subjects(classname):
-#     conn = pymysql.connect(
-#         host=app.config["MYSQL_HOST"], 
-#         user=app.config["MYSQL_USER"], 
-#         password=app.config["MYSQL_PASSWORD"], 
-#         database=app.config["MYSQL_DB"]
-#     )
-#     cursor = conn.cursor()
-    
-#     try:
-#         if classname.lower() not in ["major", "minor"]:
-#             return jsonify({'error': "Invalid classification name"}), 404
-        
-#         cursor.execute("SELECT id FROM classification WHERE class = %s", {classname})
-#         class_id = cursor.fetchone()[0]
-        
-#         cursor.execute("SELECT * FROM subjects WHERE classification_id = %s", {class_id})
-#         fetched_subjects  = cursor.fetchall()
-#         subjects = []
-#         for subj in fetched_subjects:
-#             subjects.append(
-#                 {
-#                     "id": subj[0], 
-#                     "name": subj[1],   
-#                     "img_filename": subj[2],
-#                     "classification_id": subj[3]              
-#                 }
-#             )
-            
-#         return jsonify(subjects), 200
-    
-#     except Exception as e:
-#         return jsonify({'error': str(e)}), 500
-    
-#     finally: 
-#         cursor.close()
-#         conn.close()        
-        
 @app.route('/subjects', methods=['POST'])
 def create_subject():
     name = request.form['name']
@@ -291,6 +252,153 @@ def create_task(subject_id):
         cursor.close()
         conn.close()
 
+@app.route('/subjects/<int:id>', methods=['PATCH'])
+def edit_subject(id):
+    name = request.form.get('name')
+    classname = request.form.get('classname')
+    image = request.files.get('image')
+        
+    if image is None or classname is None or name is None:
+        return jsonify({'error': 'Missing required fields'}), 404
+    
+    # grab image file name
+    img_filename = secure_filename(image.filename)
+    
+    # make img file name unique
+    img_name = str(uuid.uuid1()) + '_' + img_filename
+    
+    # build full img path
+    save_path = os.path.join(app.config['UPLOAD_FOLDER'], img_name)
+    
+    # save the img
+    image.save(save_path)
+    
+    conn = pymysql.connect(
+        host=app.config["MYSQL_HOST"], 
+        user=app.config["MYSQL_USER"], 
+        password=app.config["MYSQL_PASSWORD"], 
+        database=app.config["MYSQL_DB"]
+    )
+    cursor = conn.cursor()
+    
+    try:
+        cursor.execute(
+            """
+            UPDATE subjects
+            SET name = %s, `class` = %s, img_filename = %s
+            WHERE id = %s
+            """, (name, classname, img_name, id)
+        )
+        
+        conn.commit()
+        return jsonify({'response': 'Subject successfully edited'}), 200
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+    finally:
+        cursor.close()
+        conn.close() 
+
+@app.route('/tasks/<int:id>', methods=['PATCH'])
+def edit_task(id):
+    name = request.form['name']
+    description = request.form['description']
+    deadline = request.form['deadline']
+    
+    if deadline is None or description is None or name is None:
+        return jsonify({'error': 'Missing required fields'}), 404
+    
+    conn = pymysql.connect(
+        host=app.config["MYSQL_HOST"], 
+        user=app.config["MYSQL_USER"], 
+        password=app.config["MYSQL_PASSWORD"], 
+        database=app.config["MYSQL_DB"]
+    )
+    cursor = conn.cursor()
+    
+    try:
+        deadline_dt = datetime.strptime(deadline, '%Y-%m-%d %H:%M:%S')
+        cursor.execute(
+            """
+            UPDATE tasks
+            SET name = %s, description = %s, deadline = %s
+            WHERE id = %s
+            """, (name, description, deadline_dt, id)
+        )
+        
+        conn.commit()
+        return jsonify({'response': 'Task successfully edited!'}), 200
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+    finally:
+        cursor.close()
+        conn.close()    
+
+@app.route('/subjects/<int:id>', methods=['DELETE'])
+def delete_subject(id):
+    conn = pymysql.connect(
+        host=app.config["MYSQL_HOST"], 
+        user=app.config["MYSQL_USER"], 
+        password=app.config["MYSQL_PASSWORD"], 
+        database=app.config["MYSQL_DB"]
+    )
+    cursor = conn.cursor()
+    
+    try:
+        cursor.execute(
+            """
+            DELETE FROM tasks
+            WHERE subject_id = %s
+            """, (id,)
+        )
+        cursor.execute(
+            """
+            DELETE FROM subjects
+            WHERE id = %s
+            """, (id,)
+        )
+        
+        conn.commit()
+        return jsonify({'response': 'Subject successfully deleted'}), 200
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+    finally:
+        cursor.close()
+        conn.close() 
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    conn = pymysql.connect(
+        host=app.config["MYSQL_HOST"], 
+        user=app.config["MYSQL_USER"], 
+        password=app.config["MYSQL_PASSWORD"], 
+        database=app.config["MYSQL_DB"]
+    )
+    cursor = conn.cursor()
+    
+    try:
+        cursor.execute(
+            """
+            DELETE FROM tasks
+            WHERE id = %s
+            """, (id,)
+        )
+        
+        conn.commit()
+        return jsonify({'response': 'Task successfully edited!'}), 200
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+    finally:
+        cursor.close()
+        conn.close()    
+       
 
 
 if __name__ == '__main__':
@@ -299,3 +407,43 @@ if __name__ == '__main__':
         
     app.run(host='0.0.0.0', port=5000)
     
+    
+# @app.route('/classification/<string:classname>/subjects', methods=['GET'])
+# def get_class_subjects(classname):
+#     conn = pymysql.connect(
+#         host=app.config["MYSQL_HOST"], 
+#         user=app.config["MYSQL_USER"], 
+#         password=app.config["MYSQL_PASSWORD"], 
+#         database=app.config["MYSQL_DB"]
+#     )
+#     cursor = conn.cursor()
+    
+#     try:
+#         if classname.lower() not in ["major", "minor"]:
+#             return jsonify({'error': "Invalid classification name"}), 404
+        
+#         cursor.execute("SELECT id FROM classification WHERE class = %s", {classname})
+#         class_id = cursor.fetchone()[0]
+        
+#         cursor.execute("SELECT * FROM subjects WHERE classification_id = %s", {class_id})
+#         fetched_subjects  = cursor.fetchall()
+#         subjects = []
+#         for subj in fetched_subjects:
+#             subjects.append(
+#                 {
+#                     "id": subj[0], 
+#                     "name": subj[1],   
+#                     "img_filename": subj[2],
+#                     "classification_id": subj[3]              
+#                 }
+#             )
+            
+#         return jsonify(subjects), 200
+    
+#     except Exception as e:
+#         return jsonify({'error': str(e)}), 500
+    
+#     finally: 
+#         cursor.close()
+#         conn.close()        
+        
