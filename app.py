3b78e183012e9c60d593a18434eac386779f5032
unknown <kinlievenicedeguzman@gmail.com>
2025-06-03 00:05:30 +0800

    new backend

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ CORS(app)
 
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = "deguzman09!"
+app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"] = "todo_db"
 app.config["DEBUG"] = True
 app.config['UPLOAD_FOLDER'] = './images'
